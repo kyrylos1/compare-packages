62eab8875a1bac7c816f50eb9ef9dac542f2141f
EC2 Default User <ec2-user@ip-172-31-40-147.eu-west-1.compute.internal>
2018-04-24 12:30:24 +0000

    Added check for failed ssh connection. Changed from storing the list of packages in a file to storage in a variable

--- script.py
+++ script.py
@@ -1,6 +1,5 @@
 import argparse
 import subprocess
-import os
 import prettytable
 
 parser = argparse.ArgumentParser()
@@ -16,16 +15,19 @@ pkgsdiff = {}
 
 def package_list(server):
     try:
-        process = subprocess.run("ssh -i " + args.sshkey + " " + args.username + "@" + server + " 'test -e /etc/lsb-release'", shell=True, check=True) # Test if OS is deb based
+        process = subprocess.run("ssh -i " + args.sshkey + " " + args.username + "@" + server + " 'test -e /etc/lsb-release'", timeout=10, shell=True, check=True) # Test if OS is deb based
     except subprocess.CalledProcessError:
         process = False
+    except subprocess.TimeoutExpired:
+        print("Could not connect to the server {0}.".format(server))
+        exit()
     if process:
-        command = ["ssh -i " + args.sshkey + " " + args.username + "@" + server + " \"dpkg -l | grep '^ii'\" | awk '{print $2, $3}' > " + server] # Command for deb based OS
+        command = ["ssh -i " + args.sshkey + " " + args.username + "@" + server + " \"dpkg -l | grep '^ii'\" | awk '{print $2, $3}'"] # Command for deb based OS
     else:
-        command = ["ssh -i " + args.sshkey + " " + args.username + "@" + server + " 'rpm -qa --qf \"%{NAME} %{VERSION}\n\"' > " + server] # Command for rpm based OS
-    subprocess.run(command, shell=True)
-    with open(server) as file1:
-        serverdict = dict([line.split() for line in file1])
+        command = ["ssh -i " + args.sshkey + " " + args.username + "@" + server + " 'rpm -qa --qf \"%{NAME} %{VERSION}\n\"'"] # Command for rpm based OS
+    process = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+    process = process.stdout.decode('utf-8').rstrip().split("\n")
+    serverdict = dict([line.split() for line in process])
     return serverdict
 
 server1dict = package_list(args.server1)
@@ -54,6 +56,3 @@ print("Packages only on {0}:\n\n{1}".format(args.server2, ", ".join(pkgsonly2)))
 print("\n" + "=" * 100 + "\n")
 print("Different package versions")
 print(table)
-
-os.remove(args.server1)
-os.remove(args.server2)
