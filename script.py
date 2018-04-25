bd4f89f1a1b3d6832bc949b9394f16cc13b49821
Kyrylos1 <ec2-user@ip-172-31-40-147.eu-west-1.compute.internal>
2018-04-25 14:16:20 +0000

    Added command execution error handling

--- script.py
+++ script.py
@@ -9,26 +9,41 @@ parser.add_argument("username", help="Username for accessing the servers")
 parser.add_argument("sshkey", help="Path to ssh key for accessing the servers")
 args = parser.parse_args()
 
+CMD_TEST = 'test -e /etc/lsb-release'
+CMD_REDHAT = "rpm -qa --qf \"%{NAME} %{VERSION}\n\""
+CMD_DEB = 'dpkg -l | grep "^ii" | awk "{print \$2, \$3}"'
+
 pkgsonly1 = []
 pkgsonly2 = []
 pkgsdiff = {}
 
 def package_list(server):
+    cmd_ssh = 'ssh -i {} {}@{}'.format(args.sshkey, args.username, server)
+    cmd_test = '{} \'{}\''.format(cmd_ssh, CMD_TEST)
     try:
-        process = subprocess.run("ssh -i " + args.sshkey + " " + args.username + "@" + server + " 'test -e /etc/lsb-release'", timeout=10, shell=True, check=True) # Test if OS is deb based
-    except subprocess.CalledProcessError:
-        process = False
-    except subprocess.TimeoutExpired:
-        print("Could not connect to the server {0}.".format(server))
-        exit()
+        process = subprocess.run(cmd_test, timeout=10, shell=True, check=True, stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError as err:
+        if err.returncode == 1:
+            process = False
+        else:
+            print("Failed command execution for server {0}.\nError output: {1}\nFailed command: {2}".format(server, err.stderr.decode('utf-8').rstrip("\n"), cmd_test))
+            exit(1)
+    except subprocess.TimeoutExpired as err:
+        print("Failed command execution for server {0}.\nTimeout expired (10s).\nFailed command: {1}".format(server, err.cmd))
+        exit(1)
     if process:
-        command = ["ssh -i " + args.sshkey + " " + args.username + "@" + server + " \"dpkg -l | grep '^ii'\" | awk '{print $2, $3}'"] # Command for deb based OS
+        cmd_ = CMD_DEB
     else:
-        command = ["ssh -i " + args.sshkey + " " + args.username + "@" + server + " 'rpm -qa --qf \"%{NAME} %{VERSION}\n\"'"] # Command for rpm based OS
-    process = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+        cmd_ = CMD_REDHAT
+    cmd_packages = '{} \'{}\''.format(cmd_ssh, cmd_)
+    process = subprocess.run(cmd_packages, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if process.stderr:
+        print("Failed command execution for server {0}.\nError output: {1}\nFailed command: {2}".format(server, process.stderr.decode('utf-8').rstrip("\n"), cmd_packages))
+        exit(1)
     process = process.stdout.decode('utf-8').rstrip().split("\n")
     serverdict = dict([line.split() for line in process])
     return serverdict
+
 
 server1dict = package_list(args.server1)
 server2dict = package_list(args.server2)
@@ -56,3 +71,4 @@ print("Packages only on {0}:\n\n{1}".format(args.server2, ", ".join(pkgsonly2)))
 print("\n" + "=" * 100 + "\n")
 print("Different package versions")
 print(table)
+exit(0)
